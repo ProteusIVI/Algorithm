e7b3e6b77d190770079b44566c602e401875c010
duyen <121388050+duyenhole@users.noreply.github.com>
2023-05-05 11:03:48 +0700

    Create calculate_permutation_combination.cpp

---
+++ calculate_permutation_combination.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int fact(int n) {
+   if (n == 0 || n == 1)
+   return 1;
+   else
+   return n * fact(n - 1);
+}
+
+int main() {
+   int n, r, comb, per;
+  
+   cout<<"Enter n : ";
+   cin>>n;
+  
+   cout<<"\nEnter r : ";
+   cin>>r;
+  
+   // formula without repetition
+   comb = fact(n) / (fact(r) * fact(n-r));
+   cout << "\nCombination : " << comb;
+  
+  // formula without repetition
+   per = fact(n) / fact(n-r);
+   cout << "\nPermutation : " << per;
+  
+   return 0;
+}
